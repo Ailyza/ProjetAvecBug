dc2d079ecb72bda27dfbb1b5d060785402f3363b
Ailyza <131762373+Ailyza@users.noreply.github.com>
2023-11-27 14:44:24 +0100

    problème key

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,9 +8,12 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
 
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    (new Date(evtA.date) < new Date(evtB.date) ? -1 : 1))
-    .reverse();
+  const byDateDesc =
+		data?.focus.sort((evtA, evtB) =>
+			new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+		) || [];
+
+
  
   const nextCard = () => {
     setTimeout(
@@ -24,49 +27,49 @@ const Slider = () => {
   const changeCard = (idx) => {
     setIndex(idx);
   }
-
-  function generateUniqueKey() {
-    return Math.random().toString(36).substring(2) + Date.now().toString(36); 
-  }
-
+  // function generateUniqueKey() {
+  //   return Math.random().toString(36).substring(2) + Date.now().toString(36); 
+  // }
 
 
   return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-           key={`${event.date}-${generateUniqueKey()}`}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((radio, radioIdx) => (
-                <input
-                  key={generateUniqueKey()}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  onChange={() => changeCard(radioIdx)}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
+		<div className="SlideCardList">
+			{byDateDesc?.map((event, idx) => (
+				<>
+					<div
+						// key={`${event.date}-${generateUniqueKey()}`}
+						key={event.title}
+						className={`SlideCard SlideCard--${
+							index === idx ? "display" : "hide"
+						}`}
+					>
+						<img src={event.cover} alt="forum" />
+						<div className="SlideCard__descriptionContainer">
+							<div className="SlideCard__description">
+								<h3>{event.title}</h3>
+								<p>{event.description}</p>
+								<div>{getMonth(new Date(event.date))}</div>
+							</div>
+						</div>
+					</div>
+					<div className="SlideCard__paginationContainer">
+						<div className="SlideCard__pagination">
+							{byDateDesc.map((radio, radioIdx) => (
+								<input
+									// key={generateUniqueKey()}
+									key={radio.title}
+									type="radio"
+									name="radio-button"
+									checked={index === radioIdx}
+									onChange={() => changeCard(radioIdx)}
+								/>
+							))}
+						</div>
+					</div>
+				</>
+			))}
+		</div>
+	);
 };
 
 export default Slider;
